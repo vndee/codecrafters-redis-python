d9c4c91a18c4df573e9a17912f45f1bd6df1652e
Duy Huynh <vndee.huynh@gmail.com>
2024-11-22 10:57:06 +0700

    test

--- app/main.py
+++ app/main.py
@@ -1,17 +1,18 @@
+import os
 import socket  # noqa: F401
-
-
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    if os.name == "posix":
+        server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    else:
+        server_socket = socket.create_server(("localhost", 6379), reuse_port=False)
+    conn, addr = server_socket.accept()  # wait for client
 
-    # Uncomment this to pass the first stage
-
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-
-    conn, addr = server_socket.accept()
-    conn.sendall(b"+PONG\r\n")
-
+    while True:
+        try:
+            msg = conn.recv(1024)
+            conn.sendall(b"+PONG\r\n")
+        except ConnectionAbortedError:
+            break
 
 if __name__ == "__main__":
     main()
