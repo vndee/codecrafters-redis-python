8897157a9c46d5aae00f5594e98bdab8c3ace059
Duy Huynh <vndee.huynh@gmail.com>
2024-11-25 12:02:10 +0700

    test

--- tests/test_redis_tcp.py
+++ tests/test_redis_tcp.py
@@ -14,8 +14,9 @@ if __name__ == "__main__":
         # b"+PING\r\n",
         # b"*2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n",
         # b"*2\r\n$4\r\nLLEN\r\n$6\r\nmylist\r\n",
-        b"*5\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n$2\r\npx\r\n$3\r\n100\r\n",
-        # b"*2\r\n$3\r\nGET\r\n$3\r\nfoos\r\n",
+        # b"*5\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n$2\r\npx\r\n$3\r\n100\r\n",
+        # b"*3\r\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n",
+        b"*2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n",
         # b"*3\r\n$6\r\nCONFIG\r\n$3\r\nGET\r\n$3\r\ndir\r\n",
         # b"*2\r\n$4\r\nKEYS\r\n$1\r\n*\r\n",
     ]
